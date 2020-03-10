fa64fb27ff9a69c46f74d58a4c50d13f16c621ce
anthonyz15 <anthony.cuevas1@upr.edu>
2020-03-09 22:46:15 -0400

    CodigoFinal

--- Selection.py
+++ Selection.py
@@ -1,35 +1,33 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 
 
 class interfaz(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry("1028x768")
-        self.config(bg="green" ,bd=25,relief="sunken")
-        self.myframe=Frame(self)
+        self.geometry("1024x700")
+        self.config(bg="green", bd=25, relief="sunken")
+        self.myframe = Frame(self)
         self.myframe.pack()
-        self.myframe.config(bg="grey", width=1000, height=750)
+        self.myframe.config(bg="grey", width=1020, height=700)
 
-        #canvas = tk.Canvas(bg="grey",width=400,height=400)
-        #canvas.pack()
+        # canvas = tk.Canvas(bg="grey",width=400,height=400)
+        # canvas.pack()
         self.B1 = tk.Button(self, text="StartGame", width=16)
-        self.B1.place(x=750, y=600)
+        self.B1.place(x=750, y=500)
         self.B1.bind('<Button-1>', self.hide_me)
 
         self.B2 = tk.Button(self, text="Designer", width=16)
-        self.B2.place(x=750, y=650)
-        self.B2.bind('<Button-1>',self.hide_me)
-
-
-
+        self.B2.place(x=750, y=550)
+        self.B2.bind('<Button-1>', self.hide_me)
 
     def hide_me(self, event):
         self.Designer()
         self.B1.place_forget()
         self.B2.place_forget()
+
     def Designer(self):
         self.destroy()
         exec(open("./Designer.py").read())
@@ -42,11 +40,8 @@ class interfaz(tk.Tk):
         f.write(textToSave)
         f.close()
 
-
-        
-
     def StartGame(self):  # re center and delete
-            print("not yet")
+        print("not yet")
 
 
 if __name__ == "__main__":
